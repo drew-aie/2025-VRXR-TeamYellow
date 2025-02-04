09c598777f7e4fccf853df09376b24888549c845
Jurhell Hughes <somethinghughes@gmail.com>
2025-02-04 16:58:30 -0600

    feat: Spawner can now spawn enemies on a timer
    Also began working on difficulty progression.

--- Assets/_Project/Core/Player/GameplayManager.cs
+++ Assets/_Project/Core/Player/GameplayManager.cs
@@ -7,29 +7,61 @@ public class GameplayManager : MonoBehaviour
     [SerializeField, Tooltip("A reference to the player.")]
     private GameObject _player;
 
-    [SerializeField, Tooltip("How many hits the player can take before the game ends.")]
     private static float _playerHealth = 3f;
+    private float _healthReset;
 
+    public static bool _bGameStarted = false;
+    private static bool _bPlayerIsInvincible = false;
 
-    // Start is called before the first frame update
-    void Start()
+    //Read only property for player invincibility boolean
+    public static bool Invincible => _bPlayerIsInvincible;
+
+    public enum EDifficultyState
     {
-        
+        START,
+        EASY,
+        MEDIUM,
+        HARD,
+        END
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        _healthReset = _playerHealth;
     }
 
+    private void ResetHealth() => _playerHealth = _healthReset;
+
+    //Static function to damage the player
     public static void DamagePlayer()
     {
+        if (_bPlayerIsInvincible)
+            return;
+
         _playerHealth -= 1f;
+
+        PlayerInvincibility();
 
         if (_playerHealth == 0)
         {
-            Debug.Log("Dead");
+            TriggerEndState();
         }
+    }
+
+    //Makes the player invincible
+    public static void PlayerInvincibility() => _bPlayerIsInvincible = true;
+
+    //Ends the player's invincibility
+    public static void EndInvincibility() => _bPlayerIsInvincible = false;
+
+    public static void TriggerEndState()
+    {
+        Debug.Log("Dead");
+        //Game over stuff
+    }
+
+    private static void GameDifficulty()
+    {
+
     }
 }
