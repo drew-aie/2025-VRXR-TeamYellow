9ba1ced7eec16e0d297ab3cace3fccb6a61274ba
Trager LeMaire <142815456+TragerSL2004@users.noreply.github.com>
2025-02-07 11:32:51 -0600

    Art assets implemented, reset buttons added

--- Assets/_Project/Scripts/ScoreCounterBehaviour.cs
+++ Assets/_Project/Scripts/ScoreCounterBehaviour.cs
@@ -33,4 +33,8 @@ public class ScoreCounterBehaviour : MonoBehaviour
         _instance._currentScore += value;
     }
 
+    public void ResetScore()
+    {
+        _instance._currentScore = 0;
+    }
 }
