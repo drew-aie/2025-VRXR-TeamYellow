81d78f4c20ee6f4951f5d7efecb4d015e2f6f03b
Jurhell Hughes <somethinghughes@gmail.com>
2025-02-07 14:03:47 -0600

    fix: Fixed nav mesh and enemy facing

--- Assets/_Project/Core/Snails/SnailBehavior.cs
+++ Assets/_Project/Core/Snails/SnailBehavior.cs
@@ -50,8 +50,6 @@ public class SnailBehavior : MonoBehaviour
         if (!_snail.enabled)
             return;
 
-        Debug.Log(_target.name);
-
         //Setting agent to seek the player
         _snail.destination = _target.transform.position;
         //Making agent face the direction it's travelling using it's position and velocity
